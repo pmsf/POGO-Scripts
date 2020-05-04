e5583fb319f5ec9f962ed3eff7498ed20b9a1bf2
whitewillem <35378709+whitewillem@users.noreply.github.com>
2020-05-04 11:04:30 +0200

    initial commit

---
+++ example.config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* RDM Database */
+define('DBTYPE', 'mysql');
+define('DBUSER', 'user');
+define('DBPW', 'pass');
+define('DBHOST', 'localhost');
+define('DBNAME', 'name');
+define('CHARSET', 'utf8mb4');
+
+/* Discord Webhook */
+define('WEBHOOK', 'https://discordapp.com/api/webhooks/something');
